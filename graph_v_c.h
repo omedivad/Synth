789cac23c37e6a5ca9ac8afabd2b3d4af282ccb0
omedivad <34794182+omedivad@users.noreply.github.com>
2018-01-10 20:29:14 +0100

    Update graph_v_c.h
    fixed indentation

--- graph_v_c.h
+++ graph_v_c.h
@@ -4,15 +4,15 @@
 #define XWIN          800
 #define YWIN          600
 #define XUPL_1          0
-#define X_BTT1_2	 	   95	// x button 1 of 3
-#define X_BTT2_2	  	195	// x button 2 of 3
-#define X_BTT3_2		  290	// x button 3 of 3
-#define YUPL_1	 	 	   26
-#define XLOR_1			  400
-#define YLOR_1			  226
-#define XOSC_2  		  400
+#define X_BTT1_2       95	// x button 1 of 3
+#define X_BTT2_2      195	// x button 2 of 3
+#define X_BTT3_2      290	// x button 3 of 3
+#define YUPL_1         26
+#define XLOR_1        400
+#define YLOR_1        226
+#define XOSC_2        400
 #define BTT2          150
-#define BUFFER_LEN_G 	400
+#define BUFFER_LEN_G  400
 
 // GRAPHICAL VARAIBLES & POINTERS
 ALLEGRO_DISPLAY *display = NULL;
